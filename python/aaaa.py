60fb5e75aa23287f8554a0415b6912e7bea2b6c4
cho <josw777@naver.com>
2026-03-11 23:22:20 +0900

    판다스

--- python/aaaa.py
+++ python/aaaa.py
@@ -1,9 +1,19 @@
-﻿def calc():
-    a = 3
-    b = 5
-    def mul_add(x):
-        return a * x +b
-    return mul_add
+﻿import pandas as pd
+import numpy as np
+import random
 
-c = calc()
-print(c)
+data = [[1000,4.5],[2000,3.9],[2500,4.2]]
+idx = ['kfc','mcdonald','schoolfood']
+col=['price','rating']
+df = pd.DataFrame(data=data,index=idx,columns=col).reset_index(names='food')
+print(df)
+print("====")
+data = {
+    'kfd':[2000,5],
+    'abc':[144,4],
+    'affds':[124,555]
+}
+col=['food','price','rating']
+idx=[0,1,2]
+df = pd.DataFrame(data=data,columns=col,index=idx)
+print(df)
---
+++ python/ex/ex36.8.py
@@ -0,0 +1,29 @@
+﻿class AdvancedList(list):
+    def replace(self,old,new):
+        while old in self:
+            self[self.index(old)] = new
+
+x = AdvancedList([1, 2, 3, 1, 2, 3, 1, 2, 3])
+x.replace(1, 100)
+print(x)
+
+print("====")
+
+class Animal:
+    def eat(self):
+        print('먹다')
+
+class Wing:
+    def flap(self):
+        print('파닥거리다')
+
+class Bird(Animal,Wing):
+    def fly(self):
+        print('날다')
+
+b = Bird()
+b.eat()
+b.flap()
+b.fly()
+print(issubclass(Bird,Animal))
+print(issubclass(Bird,Wing))
